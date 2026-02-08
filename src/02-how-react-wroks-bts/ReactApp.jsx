aa589251268ba643a0265beb801ebfce7a5c89ca
neoayus <ayus43@outlook.com>
2026-02-09 00:47:10 +0530

    learn: render logic

--- src/02-how-react-wroks-bts/ReactApp.jsx
+++ src/02-how-react-wroks-bts/ReactApp.jsx
@@ -69,6 +69,20 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  console.log("RENDER!!");
+
+  function handleUndo(){
+    setShowDetails(true);
+    setLikes(0);
+  }
+  
+  function handleDelayedUndo(){
+    setTimeout(()=>{
+      setShowDetails(true);
+      setLikes(0);
+    }, 2000)
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -87,8 +101,8 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleDelayedUndo}>Undo in 2s</button>
       </div>
     </div>
   );
