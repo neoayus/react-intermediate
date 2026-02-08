a13ac552d3dce650d0655eaa333adaef8bb9a6e1
neoayus <ayus43@outlook.com>
2026-02-08 13:17:08 +0530

    fix: directly import Index in Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 // import Index from './01-usePopcorn/Index.jsx'
-import StarRating from './01-usePopcorn/StarRating.jsx'
+// import StarRating from './01-usePopcorn/StarRating.jsx'
 
 function App() {
   return (
     <>
       {/* <Index /> */}
-      <StarRating messages={["Terrible", "Bad", "Good", "Very Good", "Sodalicious"]}> </StarRating>
-      {/* <StarRating size='24'color="pink" > </StarRating> */}
     </>
   )
 }
