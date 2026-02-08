63067ab4a00017ffbff9c1ccc75a477bded243a5
neoayus <ayus43@outlook.com>
2026-02-08 17:21:19 +0530

    chore: make new dir. to understand how react works underneath

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import TextExpander from "./Challange01-TextExpander/TextExpander"
-
+import React from './HOW-REACT-WORKS-UNDERNEATH/React.jsx'
 function App() {
   return (
     <>
-      <TextExpander
-        // linkColor="red"
-        expandText="More..."
-        collapseText="Less.."
-        isExpanded="false"
-      > 
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel neque elit. Morbi posuere, neque sit amet aliquam fringilla, turpis ante faucibus justo, non fermentum diam diam et nulla. Suspendisse potenti. Curabitur nec dui nec ipsum porttitor sodales. Proin commodo efficitur vehicula. Fusce gravida ullamcorper ullamcorper. Aenean iaculis consequat quam, et commodo justo egestas eu. Vestibulum est ex, lacinia sit amet vulputate ut, iaculis sed nulla. Praesent aliquet elementum leo et convallis. Ut sodales consequat ultrices. Etiam sit amet elementum odio, ut congue neque. Cras luctus ligula ligula, id ornare elit maximus id. Quisque cursus turpis odio, a venenatis ex consectetur ut. Donec ipsum nisi, dapibus at molestie id, tempus ut erat. Donec rhoncus dui sed molestie mattis. Sed pellentesque dolor fermentum, euismod lectus id, mattis nunc. Nullam odio dui, tempor a nisi vitae, venenatis semper dolor. Phasellus ac turpis facilisis, mattis purus ac, eleifend mauris. Vivamus iaculis massa quis ante malesuada, ut eleifend arcu pellentesque. Aenean magna felis, aliquet eu ligula in, commodo sollicitudin urna.   
-      </TextExpander>
+      <React / 
     </>
   )
 }
