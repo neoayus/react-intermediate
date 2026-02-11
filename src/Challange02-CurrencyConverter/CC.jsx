192e0bf2ba461f7cea1659f76efb5e23b1a53aa9
neoayus <ayus43@outlook.com>
2026-02-11 11:52:44 +0530

    chore: finish chllange02

--- src/Challange02-CurrencyConverter/CC.jsx
+++ src/Challange02-CurrencyConverter/CC.jsx
@@ -1,21 +1,62 @@
+import { use, useEffect, useState } from "react";
+
 export default function CC(){
+  const API = "https://api.frankfurter.dev/" ;
+  // for currency conversion: https://api.frankfurter.dev/v1/latest?base=USD
+
+  const [amount, setAmount] = useState('');
+  const [currencyFrom, setCurrencyFrom] = useState('USD');
+  const [currencyTo, setCurrencyTo] = useState('INR');
+  const [converted, setConverted] = useState(0);
+  
+  // state for slow internet connectin : 
+  const [isLoading, setIsLoading] = useState(false);
+
+  // effect 
+  useEffect(function(){
+    
+    // function : fetch data from api 
+    async function convert(){
+      setIsLoading(true);
+
+      const response = await fetch(`${API}v1/latest?base=${currencyFrom}`); 
+      const data  = await response.json();
+      // console.log(data);
+      
+      setConverted((data.rates[currencyTo] * amount).toFixed(2))
+      setIsLoading(false); 
+    }
+    
+    // function call : fetch data froma api
+    if(currencyFrom == currencyTo){
+      return setConverted(amount); 
+    } 
+    convert();
+  }, [amount, currencyFrom, currencyTo])
+
   return(
     <>
       {/* input */}
-      <input type="text" />
+      <input type="number" value={amount} onChange={(e)=> setAmount( Number(e.target.value))} disabled={isLoading}/>
 
       {/* conversion : from  */}
-      <select name="" id="">
-        <option value=""></option>
+      <select name="currencyFrom" id="" value={currencyFrom} onChange={(e)=> setCurrencyFrom(e.target.value)} disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
       </select>
 
       {/* conversion : to  */}
-      <select name="" id="">
-        <option value=""></option>
+      <select name="currencyTo" id="" value={currencyTo} onChange={(e)=> setCurrencyTo(e.target.value)}disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
       </select>
       
       {/* converted currency */}
-      <p>idk</p>
+      <p>{converted} {currencyTo}</p>
     </>
   )
 }
