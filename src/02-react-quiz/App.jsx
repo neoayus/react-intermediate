7fa57599f6c6db875d51b0250d893711488d1ac4
neoayus <ayus43@outlook.com>
2026-02-12 01:10:56 +0530

    chore: initialize new project

---
+++ src/02-react-quiz/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <>
+      this that react-quiz-app.
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import App from './02-react-quiz/App.jsx' // react-quiz app
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
