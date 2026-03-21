ea1289ed0afde91f22584be6b2171b43966a0699
neoayus <ayus43@outlook.com>
2026-03-21 21:34:16 +0530

    learn: useReducer hook

--- src/02-react-quiz/App.jsx
+++ src/02-react-quiz/App.jsx
@@ -1,9 +1,50 @@
-import './index.css'
+import './Index.css';
+import Header from './imported-comonents/Header'
+import Main from './Main'
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+  questions : [],
+
+  // loading, error, ready, active, finished 
+  status: 'loading',
+};
+
+function reducer(state, action){
+  switch(action.type){
+    case 'dataReceived': 
+      return {
+        ...state, questions: action.payload,
+         status: "ready"
+      };
+    
+    case 'dataFailed': 
+      return{
+        ...state, 
+        status: 'error',  
+      };
+    
+    default : throw new Error("Action Unknown!");
+  }
+}
 
 export default function App(){
+  const[state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(()=>{
+    fetch("http://localhost:8000/questions")
+      .then(res=> res.json())
+      .then(data => dispatch({type: 'dataReceived', payload: data}))
+      .catch(err => dispatch({type: 'dataFailed'}));
+  }, []);
+
   return(
-    <>
-      this that react-quiz-app.
-    </>
+    <div className='app'>
+      <Header> </Header>
+      <Main> 
+        <p>1-14</p> 
+        <p>Question?</p>
+      </Main>
+    </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import ReactApp from './02-how-react-wroks-bts/ReactApp.jsx'
 // import ReactApp from './01-usePopcorn/ReactApp.jsx'
 // import GeoLocate from './Challange03-useGeoLocate/GeoLocate.jsx'
 
