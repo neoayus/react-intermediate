fd9bb89c1a87ba92dd93cf5c2279e6b200796691
neoayus <ayus43@outlook.com>
2026-03-25 15:17:57 +0530

    feat: add points logic to questions

--- src/02-react-quiz/App.jsx
+++ src/02-react-quiz/App.jsx
@@ -14,6 +14,8 @@ const initialState = {
   // loading, error, ready, active, finished 
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action){
@@ -35,13 +37,21 @@ function reducer(state, action){
         ...state, 
         status: 'active',  
       };
+      
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return{
+        ...state, 
+        answer: action.payload,
+        points:  action.payload === question.correctOption ? state.points + question.points : state.points ,
+      }
 
     default : throw new Error("Action Unknown!");
   }
 }
 
 export default function App(){
-  const[{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+  const[{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length; 
 
@@ -59,7 +69,7 @@ export default function App(){
         {status === "loading" && <Loader /> }
         {status === "error" && <Error /> }
         {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/> }
-        {status === "active" && <Question question={questions[index]}/> }
+        {status === "active" && <Question question={questions[index]} dispatch={dispatch} answer={answer}/> }
       </Main>
 
     </div>
--- src/02-react-quiz/components/Question.jsx
+++ src/02-react-quiz/components/Question.jsx
@@ -1,17 +1,27 @@
-export default function Question({question}){
-  // console.log(question);
-  return(
+export default function Question({ question, dispatch, answer }) {
+  // const hasAnswered ? answer != null;
+  return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question}/> 
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
-  )
+  );
 }
 
-function Options({question}){
-  return(
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
     <div className="options">
-      {question.options.map(option => <button className="btn btn" key={option}>{option}</button>)} 
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered ? (index == question.correctOption ? "correct" : "wrong") : ""}`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
